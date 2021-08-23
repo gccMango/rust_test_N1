a7bf251ae01823712089440272a854c8354bc8f5
gccMango <gcc.mango@gmail.com>
2021-08-24 00:16:03 +0900

    3rd comment

--- first_rust_project/src/main.rs
+++ first_rust_project/src/main.rs
@@ -1,3 +1,25 @@
+// enum WebEvent{
+//     //struct without fields or data types
+//     WELoad,
+//     //tuple struct with data type but no named fields
+//     WEKeys(String,char),
+//     // classic struct with named fields and their data types
+//     WEClick{x: i64,y:i64}
+
+// }
+
+// define a tuple struct
+#[derive(Debug)]
+struct KeyPress(String, char);
+
+// define a classic struct
+#[derive(Debug)]
+struct MouseClick { x: i64,y: i64}
+
+// redefine the enum variants to use the data from the new structs
+// Update the page load variant to have the boolean type
+#[derive(Debug)]
+enum WebEvent { WELoad(bool), WEClick(MouseClick), WEKeys(KeyPress) }
 
 fn main() {
     println!("Hello, world!");
@@ -19,4 +41,20 @@ fn main() {
 
     println!("{}, level {}. Remote:{} Grade: {},{},{},{}. Average: {}",user_1.name,user_1.level,user_1.remote,mark_1.0,mark_1.1,mark_1.2,mark_1.3,mark_1.4);
     println!("{}, level {}. Remote:{} Grade: {},{},{},{}. Average: {}",user_2.name,user_2.level,user_2.remote,mark_2.0,mark_2.1,mark_2.2,mark_2.3,mark_2.4);
+
+
+    let click = MouseClick{ x: 100, y: 250};
+    println!("Mouse click location : {},{}", click.x,click.y);
+
+    let keys = KeyPress(String::from ("Ctrl+"),'N');
+    println!("\nKeys pressed : {}{}",keys.0,keys.1);
+
+    let we_load = WebEvent::WELoad(true);
+
+    let we_click = WebEvent:: WEClick(click);
+
+    let we_key = WebEvent::WEKeys(keys);
+
+    println!("\nWebEvent enum structure: \n\n {:#?} \n\n {:#?} \n\n {:#?}", we_load, we_click, we_key); //we_click, we_key);
+  
 }
--- first_rust_project/target/debug/deps/first_rust_project.d
+++ first_rust_project/target/debug/deps/first_rust_project.d
@@ -1,5 +1,5 @@
-D:\rust_test_N1\first_rust_project\target\debug\deps\first_rust_project.exe: src\main.rs
+d:\rust_test_N1\first_rust_project\target\debug\deps\first_rust_project.exe: src\main.rs
 
-D:\rust_test_N1\first_rust_project\target\debug\deps\first_rust_project.d: src\main.rs
+d:\rust_test_N1\first_rust_project\target\debug\deps\first_rust_project.d: src\main.rs
 
 src\main.rs:
